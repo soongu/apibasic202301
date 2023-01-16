71d52a22413e322f27423c3c1b643a9e8ed572fe
soongu <naong@naver.com>
2023-01-16 11:55:42 +0900

    # database connect configuration

--- src/test/java/com/example/apibasic/ApibasicApplicationTests.java
+++ src/test/java/com/example/apibasic/ApibasicApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.apibasic;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class ApibasicApplicationTests {
 
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
 	@Test
-	void contextLoads() {
+	void dbConnectTest() {
+		String sql = "SELECT NOW() AS now FROM dual";
+		String now = jdbcTemplate.queryForObject(sql, String.class);
+
+		System.out.println("now = " + now);
 	}
 
 }
